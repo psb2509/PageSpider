8b9adcb5b631f378cb33e0f885c2a8de06759168
Pradeep Singh <pradeep.bisht0303@gmail.com>
2018-10-28 12:18:33 -0400

    Initial commit .

---
+++ utilities/url_utilities.py
@@ -0,0 +1,11 @@
+def load_urls_from_file(file_path:str):
+    #TODO: add the code needed to read the text files with urls in it.
+    pass
+
+def load_page(url:str):
+    #TODO: add the code that reads the url
+    pass
+
+def scrape_page(page_contents:str):
+    #TODO: analyse the text
+    pass
